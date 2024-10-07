29a271addd0406bf4649c938b31e78c9da88368f
SetterUk <167607085+SetterUk@users.noreply.github.com>
2024-10-07 09:59:33 +0530

    Create Graph.js

---
+++ src/components/Graph.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Graph = () => {
+  return (
+    <div>
+      <h1>Graph</h1>
+      <p>This page contains information about Graph data structures.</p>
+    </div>
+  );
+};
+
+export default Graph;
