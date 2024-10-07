1577d56c7d9f16c2d830ca21e50ee4648c3d8e44
SetterUk <167607085+SetterUk@users.noreply.github.com>
2024-10-07 09:58:15 +0530

    Create Stacks.js

---
+++ src/components/Stacks.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Stacks = () => {
+  return (
+    <div>
+      <h1>Stacks</h1>
+      <p>This page contains information about the Stack data structure.</p>
+    </div>
+  );
+};
+
+export default Stacks;
