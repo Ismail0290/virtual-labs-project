06f87d40ef355eb0041cb2302dfb38676d988f84
SetterUk <167607085+SetterUk@users.noreply.github.com>
2024-10-07 10:02:12 +0530

    Create Queue.js

---
+++ src/components/Queue.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Queue = () => {
+  return (
+    <div>
+      <h1>Queue</h1>
+      <p>This page contains information about the Queue data structure.</p>
+    </div>
+  );
+};
+
+export default Queue;
