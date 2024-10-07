1d0921c8ae30850074caa3d1ed20aaab7de13a84
SetterUk <167607085+SetterUk@users.noreply.github.com>
2024-10-07 10:02:50 +0530

    Create Tree.js

---
+++ src/components/Tree.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Tree = () => {
+  return (
+    <div>
+      <h1>Tree</h1>
+      <p>This page contains information about Tree data structures.</p>
+    </div>
+  );
+};
+
+export default Tree;
